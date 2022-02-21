3de4b41a3f0b38d5671809920042ffe9fa9e68f7
EdwinTsai <edwin901231@gmail.com>
2022-02-21 13:50:52 +0800

    update

--- Chapter3/main.py
+++ Chapter3/main.py
@@ -19,7 +19,7 @@ def bitplane(image , plane):
         cv.imshow(str(i), temp_plane[:, :, i])
         cv.imwrite('output/' + str(i) + '.png', plane[:, :, i])
 
-def laplacian_filter(image) :
+def laplacian_filter2(image) :
     m, n = image.shape
     filter = np.zeros((m, n, 2), dtype=np.float32)
 
@@ -37,7 +37,18 @@ def laplacian_filter(image) :
 
     return img_back
 
-
+def laplacian_filter1(image):
+    laplician = np.array(([1,1,1],[1,-8,1],[1,1,1]), dtype=float)
+    w,h = image.shape
+    pad_image = np.zeros((w+2 , h+2))
+    pad_image[1:-1 , 1:-1] = image
+    new_image = np.zeros(image.shape)
+    for i in range(w) :
+        for j in range(h):
+            temp = np.array(pad_image[i:i+3 , j:j+3])
+            result = np.dot(temp , laplician)
+            new_image[i,j] = sum(sum(result))
+    return new_image
 
 image = cv.imread('input/100-dollars.tif' , 0)
 height, width = image.shape
@@ -46,7 +57,7 @@ mask = np.zeros((height , width) ,dtype=numpy.uint8)
 cv.imshow("original" , image)
 cv.imwrite("output/original_100-dollars.png" , image)
 
-bitplane(image, plane);
+bitplane(image, plane)
 
 pic_reconstruct = reconstruct(plane[:,:,7] , plane[:,:,6] , plane[:,:,5])
 cv.imshow("reconstruct", pic_reconstruct)
@@ -55,10 +66,13 @@ cv.imwrite("output/reconstruct.png" , pic_reconstruct)
 image2 = cv.imread('input/blurry_moon.tif')
 cv.imwrite('output/original_moon.png' , image2)
 image2 = cv.cvtColor(image2, cv.COLOR_BGR2GRAY)
-lap_image = laplacian_filter(image2)
-imgplot = plt.imshow(lap_image, cmap="gray")
+lap_image2 = laplacian_filter2(image2)
+lap_image1 = laplacian_filter1(image2)
+imgplot2 = plt.imshow(lap_image2, cmap="gray")
+imgplot1 = plt.imshow(lap_image1, cmap='gray')
 plt.show()
-plt.imsave('output/laplacian.png',lap_image,cmap="gray")
+plt.imsave('output/laplacian1.png',lap_image1,cmap="gray")
+plt.imsave('output/laplacian2.png',lap_image2,cmap="gray")
 
 cv.waitKey(0)
 cv.destroyAllWindows()
